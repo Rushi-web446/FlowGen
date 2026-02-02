c304ae8f4311446ae7180603ca3684e7efb8be42
Rushi <rushidani2006@gmail.com>
2026-02-02 17:07:55 +0530

    handeling connection

--- backend/redis/connection.js
+++ backend/redis/connection.js
@@ -1,15 +1,18 @@
 const { Redis } = require("ioredis");
 
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is not defined in environment variables");
+}
+
 const redisConnection = new Redis(process.env.REDIS_URL, {
   tls: {},
-  family: 4,                  // FORCE IPv4 only.
+  // family: 4,                  // FORCE IPv4 only.
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
   reconnectOnError: () => true,
   retryStrategy(times) {
     return Math.min(times * 500, 5000);
   },
-
 });
 
 redisConnection.on("connect", () => {
